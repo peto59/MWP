491005ca9af24bee875693d1813640060a9956a0
SightMan3 <Lukasran28@gmail.com>
2023-11-11 16:45:53 +0100

    Tag manager ui almost done

--- Resources/Resource.designer.cs
+++ Resources/Resource.designer.cs
@@ -3497,25 +3497,28 @@ namespace Ass_Pain
 			public const int rounded_light = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int rounded_view_white = 2131165343;
+			public const int rounded_light_20px = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int settings = 2131165344;
+			public const int rounded_view_white = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int share = 2131165345;
+			public const int settings = 2131165345;
 			
 			// aapt resource value: 0x7F0700A2
-			public const int shuffle2 = 2131165346;
+			public const int share = 2131165346;
 			
 			// aapt resource value: 0x7F0700A3
-			public const int side_nav_bar = 2131165347;
+			public const int shuffle2 = 2131165347;
 			
 			// aapt resource value: 0x7F0700A4
-			public const int tooltip_frame_dark = 2131165348;
+			public const int side_nav_bar = 2131165348;
 			
 			// aapt resource value: 0x7F0700A5
-			public const int tooltip_frame_light = 2131165349;
+			public const int tooltip_frame_dark = 2131165349;
+			
+			// aapt resource value: 0x7F0700A6
+			public const int tooltip_frame_light = 2131165350;
 			
 			static Drawable()
 			{
@@ -4310,8 +4313,8 @@ namespace Ass_Pain
 			// aapt resource value: 0x7F080109
 			public const int snapMargins = 2131230985;
 			
-			// aapt resource value: 0x7F08010F
-			public const int songs_fragment_main = 2131230991;
+			// aapt resource value: 0x7F080110
+			public const int songs_fragment_main = 2131230992;
 			
 			// aapt resource value: 0x7F08010A
 			public const int song_cover = 2131230986;
@@ -4320,28 +4323,31 @@ namespace Ass_Pain
 			public const int song_cover_name = 2131230987;
 			
 			// aapt resource value: 0x7F08010C
-			public const int song_to_download_album = 2131230988;
+			public const int song_cover_tag_manager = 2131230988;
 			
 			// aapt resource value: 0x7F08010D
-			public const int song_to_download_artist = 2131230989;
+			public const int song_to_download_album = 2131230989;
 			
 			// aapt resource value: 0x7F08010E
-			public const int song_to_download_name = 2131230990;
+			public const int song_to_download_artist = 2131230990;
 			
-			// aapt resource value: 0x7F080110
-			public const int spacer = 2131230992;
+			// aapt resource value: 0x7F08010F
+			public const int song_to_download_name = 2131230991;
 			
 			// aapt resource value: 0x7F080111
-			public const int split_action_bar = 2131230993;
+			public const int spacer = 2131230993;
 			
 			// aapt resource value: 0x7F080112
-			public const int src_atop = 2131230994;
+			public const int split_action_bar = 2131230994;
 			
 			// aapt resource value: 0x7F080113
-			public const int src_in = 2131230995;
+			public const int src_atop = 2131230995;
 			
 			// aapt resource value: 0x7F080114
-			public const int src_over = 2131230996;
+			public const int src_in = 2131230996;
+			
+			// aapt resource value: 0x7F080115
+			public const int src_over = 2131230997;
 			
 			// aapt resource value: 0x7F08000F
 			public const int SS_image_loading = 2131230735;
@@ -4355,203 +4361,227 @@ namespace Ass_Pain
 			// aapt resource value: 0x7F080012
 			public const int SS_orignal_artist = 2131230738;
 			
-			// aapt resource value: 0x7F080115
-			public const int start = 2131230997;
-			
 			// aapt resource value: 0x7F080116
-			public const int status_bar_latest_event_content = 2131230998;
+			public const int start = 2131230998;
 			
 			// aapt resource value: 0x7F080117
-			public const int stretch = 2131230999;
+			public const int status_bar_latest_event_content = 2131230999;
 			
 			// aapt resource value: 0x7F080118
-			public const int submenuarrow = 2131231000;
+			public const int stretch = 2131231000;
 			
 			// aapt resource value: 0x7F080119
-			public const int submit_alias = 2131231001;
+			public const int submenuarrow = 2131231001;
 			
 			// aapt resource value: 0x7F08011A
-			public const int submit_area = 2131231002;
+			public const int submit_alias = 2131231002;
 			
 			// aapt resource value: 0x7F08011B
-			public const int submit_plas = 2131231003;
+			public const int submit_area = 2131231003;
+			
+			// aapt resource value: 0x7F08011C
+			public const int submit_plas = 2131231004;
 			
 			// aapt resource value: 0x7F080013
 			public const int SYM = 2131230739;
 			
-			// aapt resource value: 0x7F08011C
-			public const int tabMode = 2131231004;
-			
 			// aapt resource value: 0x7F08011D
-			public const int tag_accessibility_actions = 2131231005;
-			
-			// aapt resource value: 0x7F08011E
-			public const int tag_accessibility_clickable_spans = 2131231006;
-			
-			// aapt resource value: 0x7F08011F
-			public const int tag_accessibility_heading = 2131231007;
-			
-			// aapt resource value: 0x7F080120
-			public const int tag_accessibility_pane_title = 2131231008;
-			
-			// aapt resource value: 0x7F080121
-			public const int tag_manager_main = 2131231009;
-			
-			// aapt resource value: 0x7F080122
-			public const int tag_on_apply_window_listener = 2131231010;
-			
-			// aapt resource value: 0x7F080123
-			public const int tag_on_receive_content_listener = 2131231011;
-			
-			// aapt resource value: 0x7F080124
-			public const int tag_on_receive_content_mime_types = 2131231012;
-			
-			// aapt resource value: 0x7F080125
-			public const int tag_screen_reader_focusable = 2131231013;
-			
-			// aapt resource value: 0x7F080126
-			public const int tag_state_description = 2131231014;
-			
-			// aapt resource value: 0x7F080127
-			public const int tag_transition_group = 2131231015;
-			
-			// aapt resource value: 0x7F080128
-			public const int tag_unhandled_key_event_manager = 2131231016;
-			
-			// aapt resource value: 0x7F080129
-			public const int tag_unhandled_key_listeners = 2131231017;
-			
-			// aapt resource value: 0x7F08012A
-			public const int tag_window_insets_animation_callback = 2131231018;
-			
-			// aapt resource value: 0x7F08012B
-			public const int text = 2131231019;
+			public const int tabMode = 2131231005;
 			
 			// aapt resource value: 0x7F08012C
-			public const int text2 = 2131231020;
-			
-			// aapt resource value: 0x7F080131
-			public const int textinput_counter = 2131231025;
-			
-			// aapt resource value: 0x7F080132
-			public const int textinput_error = 2131231026;
-			
-			// aapt resource value: 0x7F080133
-			public const int textinput_helper_text = 2131231027;
+			public const int tagmngr_alau_field = 2131231020;
 			
 			// aapt resource value: 0x7F08012D
-			public const int textSpacerNoButtons = 2131231021;
+			public const int tagmngr_album_field = 2131231021;
 			
 			// aapt resource value: 0x7F08012E
-			public const int textSpacerNoTitle = 2131231022;
+			public const int tagmngr_album_label = 2131231022;
 			
 			// aapt resource value: 0x7F08012F
-			public const int textStart = 2131231023;
+			public const int tagmngr_aual_label = 2131231023;
 			
 			// aapt resource value: 0x7F080130
-			public const int text_input_password_toggle = 2131231024;
+			public const int tagmngr_author_field = 2131231024;
+			
+			// aapt resource value: 0x7F080131
+			public const int tagmngr_author_label = 2131231025;
+			
+			// aapt resource value: 0x7F080132
+			public const int tagmngr_title_field = 2131231026;
+			
+			// aapt resource value: 0x7F080133
+			public const int tagmngr_title_label = 2131231027;
+			
+			// aapt resource value: 0x7F08011E
+			public const int tag_accessibility_actions = 2131231006;
+			
+			// aapt resource value: 0x7F08011F
+			public const int tag_accessibility_clickable_spans = 2131231007;
+			
+			// aapt resource value: 0x7F080120
+			public const int tag_accessibility_heading = 2131231008;
+			
+			// aapt resource value: 0x7F080121
+			public const int tag_accessibility_pane_title = 2131231009;
+			
+			// aapt resource value: 0x7F080122
+			public const int tag_manager_main = 2131231010;
+			
+			// aapt resource value: 0x7F080123
+			public const int tag_on_apply_window_listener = 2131231011;
+			
+			// aapt resource value: 0x7F080124
+			public const int tag_on_receive_content_listener = 2131231012;
+			
+			// aapt resource value: 0x7F080125
+			public const int tag_on_receive_content_mime_types = 2131231013;
+			
+			// aapt resource value: 0x7F080126
+			public const int tag_screen_reader_focusable = 2131231014;
+			
+			// aapt resource value: 0x7F080127
+			public const int tag_state_description = 2131231015;
+			
+			// aapt resource value: 0x7F080128
+			public const int tag_transition_group = 2131231016;
+			
+			// aapt resource value: 0x7F080129
+			public const int tag_unhandled_key_event_manager = 2131231017;
+			
+			// aapt resource value: 0x7F08012A
+			public const int tag_unhandled_key_listeners = 2131231018;
+			
+			// aapt resource value: 0x7F08012B
+			public const int tag_window_insets_animation_callback = 2131231019;
 			
 			// aapt resource value: 0x7F080134
-			public const int time = 2131231028;
+			public const int text = 2131231028;
 			
 			// aapt resource value: 0x7F080135
-			public const int title = 2131231029;
-			
-			// aapt resource value: 0x7F080136
-			public const int titleDividerNoCustom = 2131231030;
-			
-			// aapt resource value: 0x7F080137
-			public const int title_template = 2131231031;
-			
-			// aapt resource value: 0x7F080139
-			public const int toolbar = 2131231033;
+			public const int text2 = 2131231029;
 			
 			// aapt resource value: 0x7F08013A
-			public const int top = 2131231034;
+			public const int textinput_counter = 2131231034;
 			
 			// aapt resource value: 0x7F08013B
-			public const int topPanel = 2131231035;
+			public const int textinput_error = 2131231035;
 			
 			// aapt resource value: 0x7F08013C
-			public const int touch_outside = 2131231036;
+			public const int textinput_helper_text = 2131231036;
+			
+			// aapt resource value: 0x7F080136
+			public const int textSpacerNoButtons = 2131231030;
+			
+			// aapt resource value: 0x7F080137
+			public const int textSpacerNoTitle = 2131231031;
 			
 			// aapt resource value: 0x7F080138
-			public const int to_download_song_image = 2131231032;
+			public const int textStart = 2131231032;
+			
+			// aapt resource value: 0x7F080139
+			public const int text_input_password_toggle = 2131231033;
 			
 			// aapt resource value: 0x7F08013D
-			public const int transition_current_scene = 2131231037;
+			public const int time = 2131231037;
 			
 			// aapt resource value: 0x7F08013E
-			public const int transition_layout_save = 2131231038;
+			public const int title = 2131231038;
 			
 			// aapt resource value: 0x7F08013F
-			public const int transition_position = 2131231039;
+			public const int titleDividerNoCustom = 2131231039;
 			
 			// aapt resource value: 0x7F080140
-			public const int transition_scene_layoutid_cache = 2131231040;
-			
-			// aapt resource value: 0x7F080141
-			public const int transition_transform = 2131231041;
+			public const int title_template = 2131231040;
 			
 			// aapt resource value: 0x7F080142
-			public const int trusted_network_label = 2131231042;
+			public const int toolbar = 2131231042;
 			
 			// aapt resource value: 0x7F080143
-			public const int trusted_network_list = 2131231043;
+			public const int top = 2131231043;
 			
 			// aapt resource value: 0x7F080144
-			public const int tursted_network_header = 2131231044;
+			public const int topPanel = 2131231044;
 			
 			// aapt resource value: 0x7F080145
-			public const int @unchecked = 2131231045;
+			public const int touch_outside = 2131231045;
+			
+			// aapt resource value: 0x7F080141
+			public const int to_download_song_image = 2131231041;
 			
 			// aapt resource value: 0x7F080146
-			public const int uniform = 2131231046;
+			public const int transition_current_scene = 2131231046;
 			
 			// aapt resource value: 0x7F080147
-			public const int unlabeled = 2131231047;
+			public const int transition_layout_save = 2131231047;
 			
 			// aapt resource value: 0x7F080148
-			public const int up = 2131231048;
+			public const int transition_position = 2131231048;
 			
 			// aapt resource value: 0x7F080149
-			public const int useLogo = 2131231049;
+			public const int transition_scene_layoutid_cache = 2131231049;
 			
 			// aapt resource value: 0x7F08014A
-			public const int user_author = 2131231050;
+			public const int transition_transform = 2131231050;
 			
 			// aapt resource value: 0x7F08014B
-			public const int view_offset_helper = 2131231051;
+			public const int trusted_network_label = 2131231051;
 			
 			// aapt resource value: 0x7F08014C
-			public const int view_tree_lifecycle_owner = 2131231052;
+			public const int trusted_network_list = 2131231052;
 			
 			// aapt resource value: 0x7F08014D
-			public const int view_tree_saved_state_registry_owner = 2131231053;
+			public const int tursted_network_header = 2131231053;
 			
 			// aapt resource value: 0x7F08014E
-			public const int visible = 2131231054;
+			public const int @unchecked = 2131231054;
 			
 			// aapt resource value: 0x7F08014F
-			public const int visible_removing_fragment_view_tag = 2131231055;
+			public const int uniform = 2131231055;
 			
 			// aapt resource value: 0x7F080150
-			public const int webview = 2131231056;
+			public const int unlabeled = 2131231056;
 			
 			// aapt resource value: 0x7F080151
-			public const int withText = 2131231057;
+			public const int up = 2131231057;
 			
 			// aapt resource value: 0x7F080152
-			public const int wrap_content = 2131231058;
+			public const int useLogo = 2131231058;
 			
 			// aapt resource value: 0x7F080153
-			public const int yes_daddy = 2131231059;
+			public const int user_author = 2131231059;
+			
+			// aapt resource value: 0x7F080154
+			public const int view_offset_helper = 2131231060;
+			
+			// aapt resource value: 0x7F080155
+			public const int view_tree_lifecycle_owner = 2131231061;
+			
+			// aapt resource value: 0x7F080156
+			public const int view_tree_saved_state_registry_owner = 2131231062;
+			
+			// aapt resource value: 0x7F080157
+			public const int visible = 2131231063;
+			
+			// aapt resource value: 0x7F080158
+			public const int visible_removing_fragment_view_tag = 2131231064;
+			
+			// aapt resource value: 0x7F080159
+			public const int webview = 2131231065;
+			
+			// aapt resource value: 0x7F08015A
+			public const int withText = 2131231066;
+			
+			// aapt resource value: 0x7F08015B
+			public const int wrap_content = 2131231067;
+			
+			// aapt resource value: 0x7F08015C
+			public const int yes_daddy = 2131231068;
 			
 			// aapt resource value: 0x7F080014
 			public const int YoutubeFragmentMainRelative = 2131230740;
 			
-			// aapt resource value: 0x7F080154
-			public const int you_are_not_sure = 2131231060;
+			// aapt resource value: 0x7F08015D
+			public const int you_are_not_sure = 2131231069;
 			
 			static Id()
 			{
--- TagManagerFragment.cs
+++ TagManagerFragment.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Collections.Generic;
 using Android.Content;
 using Android.Content.Res;
 using Android.Graphics;
+using Android.Graphics.Drawables;
 using Android.OS;
 using Android.Views;
 using Android.Widget;
 using AndroidX.Fragment.App;
+using Java.IO;
 
 namespace Ass_Pain
 {
@@ -18,8 +21,15 @@ namespace Ass_Pain
         private RelativeLayout? mainLayout;
         private Typeface? font;
         private AssetManager? assets;
-        
-            
+
+        private ImageView? songCover;
+
+        private EditText? titleIn;
+        private EditText? albumIn;
+        private EditText? authorIn;
+        private EditText? alauIn;
+    
+
         /// <inheritdoc cref="context"/>
         public TagManagerFragment(Context ctx, AssetManager? assets)
         {
@@ -30,7 +40,7 @@ namespace Ass_Pain
         }
 
 
-
+    
 
         /// <inheritdoc />
         public override View? OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -39,7 +49,47 @@ namespace Ass_Pain
 
             mainLayout = view?.FindViewById<RelativeLayout>(Resource.Id.tag_manager_main);
 
+            
+          
+
+            SetGenericFont<TextView>(view, Resource.Id.tagmngr_album_label);
+            SetGenericFont<TextView>(view, Resource.Id.tagmngr_aual_label);
+            SetGenericFont<TextView>(view, Resource.Id.tagmngr_author_label);
+            SetGenericFont<TextView>(view, Resource.Id.tagmngr_title_label);
+            
+            SetGenericFont<TextView>(view, Resource.Id.tagmngr_alau_field);
+            SetGenericFont<TextView>(view, Resource.Id.tagmngr_album_field);
+            SetGenericFont<TextView>(view, Resource.Id.tagmngr_author_field);
+            SetGenericFont<TextView>(view, Resource.Id.tagmngr_title_field);
+
+            
+            songCover = view?.FindViewById<ImageView>(Resource.Id.song_cover_tag_manager);
+            try
+            {
+                var stream = assets?.Open("music_placeholder.png");  
+                var imgBitmap = BitmapFactory.DecodeStream(stream);  
+                songCover?.SetImageBitmap(imgBitmap);
+            }
+            catch (IOException e)
+            {
+                return view;
+            }
+            
             return view;
         }
+
+
+        private void SetFont(View? view, int id)
+        {
+            TextView? label = view?.FindViewById<TextView>(id);
+            if (label != null) label.Typeface = font;
+        }
+
+        private void SetGenericFont<T>(View? view, int id)
+        {
+            if (view?.FindViewById(id) is TextView label) label.Typeface = font;
+            if (view?.FindViewById(id) is EditText input) input.Typeface = font;
+        }
+
     }
 }
